5cc3fceb31db85891f6c31691fbd5c8d6c89131b
AndreCampanholo <paschoaliniandre@gmail.com>
2025-09-12 16:24:04 -0300

    atualize

--- lab/codigos-fonte/volta_usp/volta_usp_iterativo.c
+++ lab/codigos-fonte/volta_usp/volta_usp_iterativo.c
@@ -1,3 +1,6 @@
+// 16907251 - Gustavo Vieira Gomes
+// 14558061 - André Campanholo Paschoalini
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,24 +28,39 @@ int main() {
     usp = (int *)calloc(100, sizeof(int));
     externo = (int *)calloc(100, sizeof(int));
 
+	//inicializa contadores para a quantidade de corredores da usp
+	//e a quantidade de corredores externos
     int i = 0, cont_usp = 0, cont_ext = 0;
+    
+    //Leitura das strings até EOF
     while(fgets(participantes[i], 49, stdin) != NULL) {
+		//remoção dos espaços
         remove_spaces(participantes[i], strlen(participantes[i]));
-
+        
+		//scaneia uma substring nome a partir da string original
         char nome[35];
         sscanf(participantes[i], "%[^-]", nome);
+        
+        //conta o comprimento do nome
+        int len_nome = 0;
+	      for(int i = 0; nome[i] != '\0'; i++) {
+				     len_nome++;
+	      }
 
+		//se identificar que o aluno é da usp, insere na primeira posição livre do array
+		//usp[] e incrementa qtd de corredores da usp. Caso contrário, faz o contrário.
         if(strstr(participantes[i], "usp")) {
-            usp[cont_usp] = strlen(nome);
+            usp[cont_usp] = len_nome;
             cont_usp++;
         } else {
-            externo[cont_ext] = strlen(nome);
+            externo[cont_ext] = len_nome;
             cont_ext++;
         }
 
         i++;
     }
 
+	//imprime segundo template exigido
     printf("USP - [");
     for(int i = 0; i < cont_usp; i++) {
         if(i < cont_usp - 1) {
@@ -61,6 +79,7 @@ int main() {
         }
     }
 
+	//liberação de memória
     free(externo);
     free(usp);
     for(int i = 0; i < 100; i++) {
